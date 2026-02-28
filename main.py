8e0af6dd0cfcbc515cf21782df101cf65050674c
Domwilson12 <Domwilson121012@gmail.com>
2026-02-27 20:34:28 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-print("If you see this, Python is running.")
+print("Bot started.")
 
 import time
+import yfinance as yf
+import pandas as pd
+
+print("Libraries imported successfully.")
+
 while True:
     print("Alive...")
-    time.sleep(10)
+    time.sleep(60)
